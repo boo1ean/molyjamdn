5b214cdf79d54bfefafeac43d7490fa0f46f8931
boo1ean <boo1ean0807@gmail.com>
2013-07-07 00:15:23 +0300

    Add wall

--- src/js/data/boxData.js
+++ src/js/data/boxData.js
@@ -13,7 +13,19 @@ define(
       "zone": false,
       "type": "Rectangle",
       "id": 0
+    },
+
+    {
+      "x": 1000,
+      "y": 0,
+      "halfWidth": 20,
+      "halfHeight": 300,
+      "staticBody": true,
+      "zone": false,
+      "type": "Rectangle",
+      "id": 1
     }
+
   ],
   "joints": [],
   "canvas": {
