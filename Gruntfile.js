b012ed40cdc81b9d814a324140b18542793616e1
boo1ean <boo1ean0807@gmail.com>
2013-07-06 15:42:57 +0300

    Fix coffee build issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function(grunt) {
     },
     watch: {
       game: {
-        files: ['src/**/*.js', 'src/**/*.coffee', 'styles/**/*.css', 'config.js', 'index.html'],
+        files: ['src/*.js', 'src/**/*.coffee', 'styles/**/*.css', 'config.js', 'index.html'],
         tasks: ['coffee'],
         options: {
           livereload: true
