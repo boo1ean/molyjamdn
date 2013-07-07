de100c553c406f7797af43c6965abe87a72acfbc
Yulian Konchunas <konchunas@trueconf.ru>
2013-07-07 04:28:13 +0300

    plain textured plane level

--- src/js/data/level1Data.js
+++ src/js/data/level1Data.js
@@ -1,33 +1,65 @@
-define(['./utils/generator'], function(id) {
-	return {
-		"entities": [
-			{
-				"x": 0,
-				"y": 0,
-				"staticBody": true,
-				"zone": false,
-				"type": "Rectangle",
-				"background": 'floor.png',
-				"id": id()
-			},
-			{
-				"x": 200,
-				"y": 200,
-				"staticBody": true,
-				"zone": false,
-				"type": "Rectangle",
-				"background": 'bad.png',
-				"id": id()
-			},
-			{
-				"x": 400,
-				"y": 400,
-				"staticBody": true,
-				"zone": false,
-				"type": "Rectangle",
-				"background": 'good.png',
-				"id": id()
-			}
-		]
-	}
+define(['./utils/generator',
+		'frozen/plugins/loadImage!gfx/floor.png',
+		'frozen/plugins/loadImage!gfx/bad.png'
+	   ],
+	 function(id,floor,bad) {
+return {
+	 	"entities": [
+		{
+			"x": 0,
+			"y": 300,
+			"staticBody": true,
+			"zone": false,
+			"type": "Rectangle",
+			"id": id(),
+			"background": floor
+		},
+		{
+			"x": 350,
+			"y": 300,
+			"staticBody": true,
+			"zone": false,
+			"type": "Rectangle",
+			"id": id(),
+			"background": floor
+		},
+		{
+			"x": 700,
+			"y": 300,
+			"staticBody": true,
+			"zone": false,
+			"type": "Rectangle",
+			"id": id(),
+			"background": floor
+		},		
+		{
+			"x": 1050,
+			"y": 300,
+			"staticBody": true,
+			"zone": false,
+			"type": "Rectangle",
+			"id": id(),
+			"background": floor	
+		},
+		{
+			"x": 1400,
+			"y": 300,
+			"staticBody": true,
+			"zone": false,
+			"type": "Rectangle",
+			"id": id(),
+			"background": floor	
+		},
+		{
+			"x": 1750,
+			"y": 300,
+			"staticBody": true,
+			"zone": false,
+			"type": "Rectangle",
+			"id": id(),
+			"background": floor	
+		},
+		],
+		"backImg": null
+	} 
 });
